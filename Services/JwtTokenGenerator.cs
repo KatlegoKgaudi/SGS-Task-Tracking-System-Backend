51f9480fd5e266b445b0360ba55f3e4e06b22ec2
KatlegoKgaudi <katlegokgaudi@gmail.com>
2025-10-24 22:23:33 +0200

    Models and Service classes for user authentication

---
+++ Services/JwtTokenGenerator.cs
@@ -0,0 +1,28 @@
+﻿using SGS.TaskTracker.Interfaces;
+using SGS.TaskTracker.Models;
+using System.Security.Claims;
+using System.Text;
+
+namespace SGS.TaskTracker.Services
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        //todo - define JWT Token Generator logic
+        public JwtTokenGenerator() { }
+
+        public string GenerateAccessToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
